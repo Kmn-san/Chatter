0a1003989ec779bdb2cef5587bfa93952c58e809
Kmn-san <kumenoshouhei0612@gmail.com>
2026-01-26 11:13:50 +0900

    add hooks for social authentication

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const ChatsTab = () => {
+    return (
+        <ScrollView
+            className='bg-surface '
+            contentInsetAdjustmentBehavior='automatic'
+        >
+            <Text className='text-white'>ChatsTab</Text>
+        </ScrollView>
+    )
+}
+
+export default ChatsTab
